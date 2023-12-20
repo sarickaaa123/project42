f26a6a7a95ff37e34a938fe21dcc1fb52a7d18fe
Sara Usevska <sarausevska42@yahoo.com>
2023-12-20 18:23:38 +0100

    first commit

---
+++ src/project42/MyClass.java
@@ -0,0 +1,8 @@
+package project42;
+
+public class MyClass {
+
+	public static void main(String[] args) {
+	Vehicle.horn();
+	}
+}
---
+++ src/project42/Vehicle.java
@@ -0,0 +1,8 @@
+package project42;
+
+public class Vehicle {
+
+	public static void horn() {
+	System.out.println("Beep");
+	}
+}
